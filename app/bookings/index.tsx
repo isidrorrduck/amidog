2d74f2490c9104519b39e34595a3e3602d1e50d3
isidrorrduck <isidrorr@duck.com>
2026-05-07 02:49:21 -0700

    Create index.tsx

---
+++ app/bookings/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function BookingsScreen() {
+  return (
+    <View className="flex-1 items-center justify-center p-4">
+      <Text className="text-lg font-bold">Bookings Screen</Text>
+    </View>
+  );
+}
