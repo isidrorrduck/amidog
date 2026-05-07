a9ea5f7434ba3121e06fa41719d5986ad3da94a9
isidrorrduck <isidrorr@duck.com>
2026-05-07 02:38:32 -0700

    Create login.tsx

---
+++ app/auth/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
