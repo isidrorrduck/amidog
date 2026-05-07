7524a4ee2db96b98431003119c84e589419b5f08
isidrorrduck <isidrorr@duck.com>
2026-05-07 02:43:36 -0700

    Create index.tsx

---
+++ app/breeders/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const BreedersScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Breeders Screen</Text>
+    </View>
+  );
+};
+
+export default BreedersScreen;
